8656412c9a0f1543a1c2470dbc48cc10d73f0aea
SaladRaider <peterkangveerman@gmail.com>
2016-06-04 16:30:57 -0700

    Added ability to post highscores for normal mode

--- highscore_submit.php
+++ highscore_submit.php
@@ -1,19 +1,17 @@
 <?php
+require('config.php');
 
-$dbc = mysqli_connect('localhost',"root",'password');
-//$dbc = mysqli_connect('localhost',"saladraider",'retep1');
-if (!$dbc)
-{
-	die('Not connected : ' . mysqli_error($dbc));
+$name = trim($dbc->real_escape_string(htmlentities($_POST['name'])));
+$score = trim($dbc->real_escape_string(htmlentities($_POST['score'])));
+$game_mode = trim($dbc->real_escape_string(htmlentities($_POST['game_mode'])));
+
+$sql = "INSERT INTO `high_scores` (`name`, `score`, `game_mode`) VALUES ('$name', '$score', '$game_mode')";
+$result = $dbc->query($sql);
+
+if($result) {
+	header("Location:highscores.php");
+} else {
+	echo "Failed: " + $sql;
 }
-
-//select database
-$db_selected = mysqli_select_db($dbc,"colorgame");
-if (!$db_selected)
-{
-	die("Can not connect : " . mysqli_error($dbc));
-}
-
-
 
 ?>
